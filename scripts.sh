4a503254b16f9f2b2458570d2f21dc470b63589c
Ibra Al Tabian <ibratabian@gmail.com>
2025-10-23 10:16:27 +0700

    Add cherry-pick for hardware lineage compat

--- scripts.sh
+++ scripts.sh
@@ -16,6 +16,8 @@ export BUILD_USERNAME=ibratabian17
 # Hax
 cd packages/modules/Connectivity 
 git fetch https://github.com/LineageOS/android_packages_modules_Connectivity refs/changes/72/452772/1 && git cherry-pick FETCH_HEAD
+cd ../../../hardware/lineage/compat
+git fetch https://github.com/yaap/hardware_lineage_compat/ && git cherry-pick ff324381288b4e097e22dc523eb2e28b5eb14f56
 cd ../../../system/core
 git remote add fix https://github.com/AxionAOSP/android_system_core && git cherry-pick 927eOd126f912c88b4253f18ab 2913b8749ab1d3
 cd ../../

98c942f047bb863b2fefec37e05c8c4a10c2a53b
Ibra Al Tabian <ibratabian@gmail.com>
2025-10-27 08:26:33 +0700

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -17,9 +17,9 @@ export BUILD_USERNAME=ibratabian17
 cd packages/modules/Connectivity 
 git fetch https://github.com/LineageOS/android_packages_modules_Connectivity refs/changes/72/452772/1 && git cherry-pick FETCH_HEAD
 cd ../../../hardware/lineage/compat
-git fetch https://github.com/yaap/hardware_lineage_compat/ && git cherry-pick ff324381288b4e097e22dc523eb2e28b5eb14f56
+git fetch https://github.com/yaap/hardware_lineage_compat/ && git checkout FETCH_HEAD
 cd ../../../system/core
-git remote add fix https://github.com/AxionAOSP/android_system_core && git cherry-pick 927eOd126f912c88b4253f18ab 2913b8749ab1d3
+git remote add fix https://github.com/AxionAOSP/android_system_core && git cherry-pick 927e0d126f912c88b4253f18ab2913b8749ab1d3
 cd ../../
 
 # Build the ROM
